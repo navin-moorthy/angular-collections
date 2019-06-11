b2d2a8898d42db41f37b1849752c1a2ee86633c6
NavinNavi19 <navin007.a@gmail.com>
2019-06-11 19:43:04 +0000

    02 Interpolation App Done

--- Collections/FeaturesExplained/02InterpolationApp/src/app/app.component.ts
+++ Collections/FeaturesExplained/02InterpolationApp/src/app/app.component.ts
@@ -2,7 +2,18 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  template: `
+    <h1>{{ title }}</h1>
+    <h2>{{ "Title: " + title }}</h2>
+    <h3>{{ 2 * 5 + 10 }}</h3>
+    <h4>{{ "Title: " + getTitle() }}</h4>
+    <h5 title="{{ title }}">Move the mouse cursor here to see the title</h5>
+  `
 })
-export class AppComponent {}
+export class AppComponent {
+  title = "Interpolation demonstration";
+
+  getTitle(): string {
+    return this.title;
+  }
+}
