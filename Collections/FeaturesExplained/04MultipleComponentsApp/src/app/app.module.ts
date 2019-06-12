741dd4b3198a0be60cad09a5581f64d50b04d785
NavinNavi19 <navin007.a@gmail.com>
2019-06-12 22:11:03 +0000

    Updated Content

---
+++ Collections/FeaturesExplained/04MultipleComponentsApp/src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
+import { AppComponent } from "./app.component";
+import { EmployeesComponent } from "./employees/employees.component";
+
+@NgModule({
+  declarations: [AppComponent, EmployeesComponent],
+  imports: [BrowserModule],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ Collections/FeaturesExplained/04MultipleComponentsApp/src/app/employees/employees.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-employees",
+  templateUrl: "./employees.component.html"
+})
+export class EmployeesComponent {
+  title = "Employees List";
+}
